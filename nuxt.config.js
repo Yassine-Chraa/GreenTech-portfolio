848af31c8c73bc836e9d73034a680f40cd9fe7b7
Yassine-Chraa <yassine.chraa1@email.com>
2022-09-06 14:54:05 +0100

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
         name: 'description',
         content:
-          "Bureau d'etude import et export marchand de produits phytosantaires",
+          "Bureau d'etude import et export marchand de produits phytosantaires.",
       },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'robots', content: 'noindex' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
