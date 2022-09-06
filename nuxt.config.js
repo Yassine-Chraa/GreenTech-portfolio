8cad314fffc2ef48106008555e3b433cc93fac34
Yassine-Chraa <yassine.chraa1@email.com>
2022-09-06 14:43:11 +0100

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'GreenTech',
+    title: 'GreenTech Consulting',
     htmlAttrs: {
       lang: 'en',
     },
